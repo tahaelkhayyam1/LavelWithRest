7a0a177879c768221f50e1d5248635a891f0f68c
taha <tahaelkhayyam1@gmail.com>
2025-03-12 13:46:57 +0000

    first commit

---
+++ database/migrations/2025_03_12_132112_create_invoices_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->integer('customer_id');
+            $table->string('amount');
+            $table->string('description');
+            $table->string('status'); // paid, unpaid
+            $table->string('billed_dated');
+                        $table->string('paid_date')->nullable();    
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
